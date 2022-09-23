229f7111a17c82f015c16384ffe32fc2ce61322c
mhame <mhame@10.0.0.42>
2022-09-23 17:31:47 -0600

    Age Calculator Created Calculator Works No validation

---
+++ src/java/Servlets/AgeCalculatorServlet.java
@@ -0,0 +1,41 @@
+
+package Servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author mhame
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp")
+                        .forward(request, response);
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String AgeS = request.getParameter("age");
+        
+        int ageI = Integer.parseInt(AgeS);
+        
+        ageI = ageI + 1;
+        
+        request.setAttribute("ageMessage", "Your age next Birthday will be: " + ageI);
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp")
+                .forward(request, response);
+    }
+
+}
---
+++ web/WEB-INF/ageCalculator.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : agecalculator
+    Created on : 21-Sep-2022, 11:43:02 AM
+    Author     : mhame
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+        <form action="age"  method="post">
+            Enter your age: <input type="number" name="age" value="">
+            <input type="submit" value="Submit">
+        </form>
+        <div> ${ageMessage}</div> <br>
+        <a href="arithmetic">
+            Arithmetic Calculator
+        </a>
+    </body>
+</html>
